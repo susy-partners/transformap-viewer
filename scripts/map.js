2644054377e77b45c010a356eae0d8813c39b731
almereyda <almereyda@allmende.io>
2018-02-15 12:52:54 +0100

    manual imitation of 4c444fe712fb4514648128adca77d8f666169e6f

--- scripts/map.js
+++ scripts/map.js
@@ -214,6 +214,12 @@ function addPOIsToMap(geoJSONfeatureCollection) {
       }
     }
 
+    var createdDate = new Date(feature.properties._created)
+    var created = createdDate.toLocaleDateString()
+
+    var timestampDate = new Date(feature.properties._timestamp)
+    var timestamp = timestampDate.toLocaleDateString()
+
     var pdata = {
       icon:  new L.divIcon({
         className: 'my-div-icon' + cats,
@@ -221,6 +227,8 @@ function addPOIsToMap(geoJSONfeatureCollection) {
         html:"<div><div>" + feature.properties.name + "</div></div>"
       }),
       popup: livePopup,
+      created: created,
+      modified: timestamp,
       tags: feature.properties,
       properties: feature.properties // is used by _ template
     }
@@ -1350,6 +1358,8 @@ var dictionary = {
     "set_filters" : "set Filters",
     "imprint" : "Imprint",
     "linked_data" : "Linked Data",
+    "creation_date" : "Date of Entry",
+    "modification_date" : "Date of Update",
     "susy_disclaimer" : "This website has been produced with the financial assistance of the European Union. The contents of this website are the sole responsibility of the SUSY initiative and can under no circumstances be regarded as reflecting the position of the European Union.",
     "" : "",
     "LAST:":""
